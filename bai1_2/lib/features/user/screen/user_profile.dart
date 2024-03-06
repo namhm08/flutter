f0325a5490a972eb526faa9aff333e2e29107bd3
namhm08 <161960081+namhm08@users.noreply.github.com>
2024-03-07 01:26:28 +0700

    avatar

--- bai1_2/lib/features/user/screen/user_profile.dart
+++ bai1_2/lib/features/user/screen/user_profile.dart
@@ -9,12 +9,47 @@ class screen_user_profile extends StatelessWidget{
         title: const Text("Profile"),
         centerTitle: true,
       ),
-      body: Center(
-        child: Image(
-          image: AssetImage('assets/img0.jpg'),
-        ),
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: MediaQuery.of(context).size.height * 0.03),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: 100,
+            child: const Center(
+              child: CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('assets/img0.jpg'),
+              ),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(5.0),
+            child: Text(
+              'Hoang Minh Nam',
+              style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          const Text('namhmhe176539@fpt.edu.vn'
+
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: MediaQuery.of(context).size.height * 0.02),
+          ),
+
+          Container(
+            color: Colors.green,
+            width: MediaQuery.of(context).size.width,
+            height: 100,
+          ),
+        ],
       ),
     );
   }
-  
+
 }
